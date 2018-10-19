2535dca97d94f534aaea991a2421c0f8d3c6414e
Omega014 <metallic.code@gmail.com>
2018-10-19 22:27:21 +0900

    can not use basecustommodels

--- apps/models.py
+++ apps/models.py
@@ -5,13 +5,6 @@ from apps.database import db
 from flask_login import UserMixin
 
 
-class BaseDateModel(db.Model):
-    """ It is a base model add utime/ctime timestamp to anothermodelsi
-    """
-    ctime = db.Column(db.DateTime, nullable=False, default=datetime.now)
-    utime = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
-
-
 class User(db.Model, BaseDateModel, UserMixin):
 
     __tablename__ = 'users'
@@ -19,6 +12,8 @@ class User(db.Model, BaseDateModel, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(50), nullable=False)
+    ctime = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    utime = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
 
     def get_id(self):
         return 1
@@ -33,6 +28,8 @@ class Question(db.Model, BaseDateModel):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
+    ctime = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    utime = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
 
 
 class Answer(db.Model, BaseDateModel):
@@ -43,6 +40,8 @@ class Answer(db.Model, BaseDateModel):
     user_id = db.Column(db.Integer, nullable=False)
     question_id = db.Column(db.Integer, nullable=False)
     text = db.Column(db.String(255))
+    ctime = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    utime = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
 
 
 class Group(db.Model, BaseDateModel):
@@ -52,6 +51,8 @@ class Group(db.Model, BaseDateModel):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     channel = db.Column(db.String(255))
+    ctime = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    utime = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
 
     def get_group(user):
         pass
