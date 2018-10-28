f567a017e891a733554a120a894a731d4658e007
Omega014 <metallic.code@gmail.com>
2018-10-28 17:28:55 +0900

    change name from group to channel

--- apps/models.py
+++ apps/models.py
@@ -28,7 +28,7 @@ class User(db.Model, UserMixin):
     ctime = db.Column(db.DateTime, nullable=False, default=datetime.now)
     utime = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
 
-    channel = db.relationship(
+    channels = db.relationship(
         'Channel',
         secondary=UserChannel.__tablename__,
         back_populates='users',
@@ -40,6 +40,27 @@ class User(db.Model, UserMixin):
 
     def get_auth_token(self):
         return hashlib.sha1(self.name + self.password).hexdigest()
+
+
+class Channel(db.Model):
+
+    __tablename__ = 'channels'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    ctime = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    utime = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
+
+    users = db.relationship(
+        'User',
+        secondary=UserChannel.__tablename__,
+        back_populates='channels',
+    )
+    user_channel = db.relationship('UserChannel')
+
+    @classmethod
+    def get_channels(cls, user):
+        return cls.query.filter(cls.id==user.id)
 
 
 class Question(db.Model):
@@ -63,24 +84,3 @@ class Answer(db.Model):
     text = db.Column(db.String(255))
     ctime = db.Column(db.DateTime, nullable=False, default=datetime.now)
     utime = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
-
-
-class Channel(db.Model):
-
-    __tablename__ = 'channels'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    ctime = db.Column(db.DateTime, nullable=False, default=datetime.now)
-    utime = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
-
-    users = db.relationship(
-        'User',
-        secondary=UserChannel.__tablename__,
-        back_populates='channels',
-    )
-    user_channel = db.relationship('UserChannel')
-
-    @classmethod
-    def get_channel(cls, user):
-        return cls.query.filter(cls.id==user.id)
