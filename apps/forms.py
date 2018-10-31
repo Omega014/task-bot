8f9ea59e999effcb69efa993ee9dc7f3790d8bb8
Omega014 <metallic.code@gmail.com>
2018-10-31 18:00:08 +0900

    add validators

--- apps/forms.py
+++ apps/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
+from wtforms.validators import InputRequired
 
 
 class QuestionForm(FlaskForm):
-    title = StringField('Question')
-
+    title = StringField('Question',  [InputRequired()])
